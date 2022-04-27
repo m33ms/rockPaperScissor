1542df3d4f155dadd057e74bcaa99915e07df3ae
mimi schirm <mimischirm@gmail.com>
2022-04-27 13:01:38 -0700

    get user input

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import random
 
 choices = ["rock", "paper", "scissors"]
 computer = random.choice(choices)
+player = input("rock, paper, or scissors?: ")
 
-print(computer)
+print("computer: ", computer)
+print("player: ", player)
